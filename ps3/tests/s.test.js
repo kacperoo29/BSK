fb683d2c440153077dc7cae8aa89f369b0d70e83
me3eh <matt30002000@gmail.com>
2022-03-17 12:31:54 +0100

    Deleted test created with js

--- ps3/tests/s.test.js
+++
@@ -1,8 +0,0 @@
- 
-const matrix_cipher_a_for_testing = require('../public/ex_2.js');
-
-describe("helloWorld", () => {
-    it("returns hello world", () => {
-      expect(matrix_cipher_a_for_testing("sa", "1-2")).toBe("hello world");
-    });
-  });
