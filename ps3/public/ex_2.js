c9347083acd092520b80fd69e46d948520ba9667
me3eh <matt30002000@gmail.com>
2022-03-19 18:58:00 +0100

    Added comments for working program

--- ps3/public/ex_2.js
+++ ps3/public/ex_2.js
@@ -1,12 +1,23 @@
 
 export function matrix_cipher_a(text, key){
-    let array_of_key = key.split("-");        
+    // parsing key to array ex. 3-1-2 to [3,1,2]
+    let array_of_key = key.split("-");
+    
+    // parsing text to encode to array
     let text_into_array = text.split("");
+
+    // return encoded string
     return matrix_cipher(text_into_array, array_of_key);
 }
+
 export function matrix_decipher_a(text, key){
+    // parsing key to array ex. 3-1-2 to [3,1,2]
     let array_of_key = key.split("-");        
+
+    // parsing text to encode to array
     let text_into_array = text.split("");
+
+    // return decoded string
     return matrix_decipher(text_into_array, array_of_key);
 }
 
@@ -32,24 +43,39 @@ export function validateKey2a(key) {
 
 function matrix_cipher(text_into_array, array_of_key){
     let ciphered = [];
+    // iterate every key length
     for( let i = 0 ; i < text_into_array.length ; i += array_of_key.length ){
+
+        // iterate through every char in key
         for( let j = 0 ; j < array_of_key.length ; ++j ){
+
+            // get number from array with keys
             let key_index = parseInt(array_of_key[ j ] ) - 1; 
+
+            // if some iteration of whole key length + key from key array index < length of string with message 
             if( i + key_index  < text_into_array.length )
+                // add to ciphered string char from index of iteration of whole key length + key from key array index
                 ciphered.push( text_into_array[ i + key_index ] );
         }
     }
+    // join arrays with "" char
     return ciphered.join("");
 }
 
 function matrix_decipher(text_in_array, array_of_key){
+
     let deciphered = new Array(text_in_array.length);
     let index = 0;
-    
+
+    // iterate every key length
     for( let i = 0 ; i < text_in_array.length ; i += array_of_key.length ){
+
+
         let how_many_keys_iterate_through = array_of_key.length;
         let duplicated_array_of_key = array_of_key;
 
+        // if decoded_string.length % key.length != 0 at the last iteration, 
+        //we need to get rid of too big keys to not meet any problems
         if( i + array_of_key.length > text_in_array.length ){
             how_many_keys_iterate_through = text_in_array.length - i;
             duplicated_array_of_key = array_of_key.filter(function(value){ 
--- ps3/public/ex_3.js
+++ ps3/public/ex_3.js
@@ -1,11 +1,15 @@
  export function matrix_cipher_b(text, key_in_text){
+    // change letters from key to numbers
     let key = alphabetIntoCode(text, key_in_text);
-
+    
+    //cipher the text 
     return matrix_cipher(text, key);
 }
 export function matrix_decipher_b(text, key_in_text){
+    // change letters from key to numbers
     let key = alphabetIntoCode(text, key_in_text);
-
+    
+    //decipher the text 
     return matrix_decipher(text, key);
 }
 
@@ -17,27 +21,49 @@ export function validateKey2b(key)
 }
 
 function matrix_cipher(text, key){
+    // get rid of all spaces - this cipher doesn't need it
     let text_in_array = text.replace(new RegExp(" ", 'g'), "");
+
+    // prepare array for cyphered word
     let cyphered_array = [];
+
+    //iterate through whole key set 
     for( let t = 1 ; t <= key.length ; ++t){
+        // find the occurence of next position in key
         let current_index = key.findIndex(e => e == t);
+        
+        //every length of key array put a char into cyphered word 
         for( let i = 0 ; i < text_in_array.length ; i += key.length )
             cyphered_array.push( text_in_array[ i + current_index] );
+
+        // add a space after each key
         cyphered_array.push(" ");
     }
+    // change array into string
     return cyphered_array.join("");
 }
 function matrix_decipher(text, key){
+    // prepare the text
     let text_splitted_by_spaces = text.split(" ");
+    
+    //find the longest word among text 
     let longest = findLongestWordLength(text_splitted_by_spaces);
-    let v = "";
+
+    // prepare string for decoded string
+    let decoded_string = "";
+
+    // iterate through length of longest word in given encoded text
     for(let i = 0 ; i < longest ; ++i){
+
+        // iterate thorugh all keys in set
         for(let j = 0 ; j < key.length; ++j){
+
+            // if length of text is smaller than iterating index, add a character from encoded string to decoded string  
             if(text_splitted_by_spaces[ key[j] - 1 ].length > i)
-                v += text_splitted_by_spaces[ key[j] - 1 ][i];
+                decoded_string += text_splitted_by_spaces[ key[j] - 1 ][i];
         }
     }
-    return v;
+    return decoded_string;
 }
 function findLongestWordLength(str) {
     return Math.max(...str.map(word => word.length));
@@ -45,13 +71,21 @@ function findLongestWordLength(str) {
 
 function alphabetIntoCode(text, key_in_text){
     let key_index = 1;
+    // key replaced every space into "" and uppercased
     let trimmed_key_in_text = key_in_text.replace(new RegExp(" ", 'g'), "").toUpperCase();
+    
+    // create new array which will have all key values 
     let key = new Array( trimmed_key_in_text.length );
+
     // ASCII numbers A=65 Z=90
     for( let i = 65 ; i <= 90 ; ++ i)
+        // if key have this letter
         if( trimmed_key_in_text.includes( String.fromCharCode(i) ) )
+            // iterate through text
             for( let j = 0 ; j < text.length ; ++j)
+                // if iterated key matched char
                 if( trimmed_key_in_text[j] == String.fromCharCode(i) )
+                    // add to newly created array incremented index
                     key[j] = key_index++;
     return key;
 }
