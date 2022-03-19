bee362ff223eaf6591dfaee0f115efe3e64bc639
erednay29 <erednay29@gmail.com>
2022-03-19 19:12:08 +0100

    Rail fence comments

--- ps3/public/railFence.js
+++ ps3/public/railFence.js
@@ -6,9 +6,11 @@ function makeArray (d1, d2) {
   return arr
 }
 
-export function encodeRailFence (text, n) {
+export function encodeRailFence(text, n) {
+  // Initialize matrix for cipher
   let rail = makeArray(n, text.length)
 
+  // Fill the matrix with "empty" character
   for (let i = 0; i < n; ++i)
     for (let j = 0; j < text.length; ++j) rail[i][j] = '\n'
 
@@ -16,13 +18,18 @@ export function encodeRailFence (text, n) {
   let row = 0,
     col = 0
   for (let i = 0; i < text.length; ++i) {
+    // Change direction if reached top or bottom
     if (row == 0 || row == n - 1) dir = !dir
 
+    // Copy character from text to right place in matrix
+    // Move to the right
     rail[row][col++] = text[i]
 
+    // Go up or down depending on direction
     dir ? ++row : --row
   }
 
+  // Copy everything that's not "empty" character to result
   let result = ''
   for (let i = 0; i < n; ++i)
     for (let j = 0; j < text.length; ++j)
@@ -31,24 +38,32 @@ export function encodeRailFence (text, n) {
   return result
 }
 
-export function decodeRailFence (text, n) {
+export function decodeRailFence(text, n) {
+  // Initialize matrix for cipher
   let rail = makeArray(n, text.length)
 
+  // Fill the matrix with "empty" character
   for (let i = 0; i < n; ++i)
     for (let j = 0; j < text.length; ++j) rail[i][j] = '\n'
 
   let dir = 0
   let row = 0,
     col = 0
+  // Mark places where character is expected in cipher with "*"
   for (let i = 0; i < text.length; ++i) {
+    // Change direction if reached top or bottom
     if (row == 0) dir = 1
     if (row == n - 1) dir = 0
 
+    // Mark the place
+    // Move to the right
     rail[row][col++] = '*'
 
+    // Go up or down depending on direction
     dir ? ++row : --row
   }
 
+  // Copy characters from cipher to expected places in matrix
   let index = 0
   for (let i = 0; i < n; ++i)
     for (let j = 0; j < text.length; ++j)
@@ -57,13 +72,18 @@ export function decodeRailFence (text, n) {
   let result = ''
   row = 0
   col = 0
+  // Iterate throught matrix and construct the result
   for (let i = 0; i < text.length; ++i) {
+    // Change direction if reached top or bottom
     if (row == 0) dir = 1
     if (row == n - 1) dir = 0
 
+    // If character was set copy it to the result
+    // Move to the right
     if (rail[row][col] != '*')
       result += rail[row][col++]
     
+    // Go up or down depending on direction
     dir ? ++row : --row
   }
 
