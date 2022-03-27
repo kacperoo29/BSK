2d1a38950d0bd064f581b740df0147548d42d989
Kacper Bajenski <erednay29@gmail.com>
2022-03-27 18:10:29 +0200

    Update site

--- ps4/public/main.js
+++ ps4/public/main.js
@@ -1,2 +1,29 @@
-import { lfsr } from 'lsfr'
+import { lfsr } from './lfsr.js'
+
+const output = document.getElementById('output')
+const key = document.getElementById('key')
+
+const startButton = document.getElementById('start')
+const stopButton = document.getElementById('stop')
+
+var state = 0
+
+startButton.addEventListener('click', () => {
+    var taps = key.value
+    output.value = ""
+    state = 1
+
+    var interval = setInterval(() => {
+        if (!state)
+            clearInterval(interval)
+        
+        var tmp = lfsr(taps)
+        console.log(tmp)
+        output.value += tmp
+    }, 500)
+})
+
+stopButton.addEventListener('click', () => {
+    state = 0
+})
 
